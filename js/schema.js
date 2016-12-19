229c033555e5d582d7ec070b7ea83a1b17c71d57
Sona Pecenakova <sona.pecenakova@gmail.com>
2016-12-19 09:12:40 +0100

    init commit

---
+++ js/schema.js
@@ -0,0 +1,30 @@
+[
+     {
+     "synsetId":       "4120489",
+     "keywords":  ["buckwheat", "bullshit"],
+     "averageSimilarity":     "0.05",
+     "numberOfImages": "240",
+     "templates": [
+     {
+     "imagePath" : "http://static.visual-computing.com/habit/n00006484/n00006484_25.jpg",
+     "imageSimilarity" : "0.928",
+     "rightCategory" : "true"
+     },
+     {
+     "imagePath" : "http://static.visual-computing.com/habit/n00006484/n00006484_25.jpg",
+     "imageSimilarity" : "0.928",
+     "rightCategory" : "true"
+     },
+     {
+     "imagePath" : "http://static.visual-computing.com/habit/n00006484/n00006484_25.jpg",
+     "imageSimilarity" : "0.928",
+     "rightCategory" : "true"
+     },
+     {
+     "imagePath" : "http://static.visual-computing.com/habit/n00006484/n00006484_25.jpg",
+     "imageSimilarity" : "0.928",
+     "rightCategory" : "true"
+     }
+     ]
+     }
+]
